e31334251b3fe25edb10c55cfa6ca12d912c1ecd
Atul-Bonde <atul.bonde@gmail.com>
2023-11-27 22:37:57 +0530

    Category Module Completed

--- src/main/java/com/bikkadit/electronicstore/controller/CategoryController.java
+++ src/main/java/com/bikkadit/electronicstore/controller/CategoryController.java
@@ -4,12 +4,23 @@ import com.bikkadit.electronicstore.constants.AppConstant;
 import com.bikkadit.electronicstore.constants.UrlConstant;
 import com.bikkadit.electronicstore.dtos.CategoryDto;
 import com.bikkadit.electronicstore.payloads.ApiResponseMessage;
+import com.bikkadit.electronicstore.payloads.ImageResponse;
 import com.bikkadit.electronicstore.payloads.PageableResponse;
 import com.bikkadit.electronicstore.service.CategoryService;
+import com.bikkadit.electronicstore.service.FileService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 
 @RestController
 @RequestMapping(UrlConstant.BASE_URL + UrlConstant.CATEGORY_URL)
@@ -17,6 +28,12 @@ public class CategoryController {
 
     @Autowired
     private CategoryService categoryService;
+
+    @Autowired
+    private FileService fileService;
+
+    @Value("${category.profile.image.path}")
+    private String path;
 
     @PostMapping("/")
     public ResponseEntity<CategoryDto> saveCategory(@RequestBody CategoryDto categoryDto) {
@@ -57,7 +74,7 @@ public class CategoryController {
     }
 
     @DeleteMapping("/{categoryId}")
-    public ResponseEntity<ApiResponseMessage> deleteCategory(@PathVariable String categoryId){
+    public ResponseEntity<ApiResponseMessage> deleteCategory(@PathVariable String categoryId) {
 
         this.categoryService.deleteCategory(categoryId);
 
@@ -66,4 +83,33 @@ public class CategoryController {
         return new ResponseEntity<>(apiResponseMessage, HttpStatus.OK);
     }
 
+    @PostMapping("/images/{categoryId}")
+    public ResponseEntity<ImageResponse> uploadCoverImage(@RequestParam MultipartFile file,@PathVariable String categoryId) throws IOException {
+
+        String ImageName = this.fileService.uploadFile(file, path);
+
+        CategoryDto categoryDto = this.categoryService.getSingleCategory(categoryId);
+
+        categoryDto.setCoverImage(ImageName);
+
+        CategoryDto categoryDto1 = this.categoryService.updateCategory(categoryDto, categoryId);
+
+        ImageResponse imageResponse = ImageResponse.builder().message("Cover Image Uploaded Successfully..").imageName(ImageName).status(true).httpStatus(HttpStatus.OK).build();
+
+        return new ResponseEntity<ImageResponse>(imageResponse, HttpStatus.OK);
+
+    }
+
+    @GetMapping("/images/{categoryId}")
+    public void serveCoverImage(@PathVariable String categoryId, HttpServletResponse response) throws IOException {
+
+        CategoryDto categoryDto = this.categoryService.getSingleCategory(categoryId);
+
+        InputStream resource = fileService.getResource(path, categoryDto.getCoverImage());
+
+        response.setContentType(MediaType.IMAGE_JPEG_VALUE);
+
+        StreamUtils.copy(resource,response.getOutputStream());
+
+    }
 }
